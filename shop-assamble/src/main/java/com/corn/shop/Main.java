6f2f2510db08348a3244ea1c4382868aacf4a703
Yyc <791621554@qq.com>
2019-04-08 17:33:46 +0800

    商城客户端用户业务

--- shop-assamble/src/main/java/com/corn/shop/Main.java
+++ shop-assamble/src/main/java/com/corn/shop/Main.java
@@ -3,8 +3,10 @@ package com.corn.shop;
 
 import com.corn.boot.annotations.Runner;
 import com.corn.boot.base.RunMain;
+import org.springframework.context.annotation.ImportResource;
 
 @Runner
+@ImportResource(value = "dubbo-admin.xml")
 public class Main {
 
     public static void main(String[] args) {
