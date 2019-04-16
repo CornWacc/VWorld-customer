352cc5e8afb9e07f55d16d646a0fadec328a9c8d
Yyc <791621554@qq.com>
2019-04-16 16:45:14 +0800

    项目更新

---
+++ vworld-controller/src/main/java/com/corn/shop/controller/user/UserController.java
@@ -0,0 +1,75 @@
+package com.corn.shop.controller.user;
+
+
+import com.corn.boot.base.JsonResult;
+import com.corn.boot.util.AppUtils;
+import com.corn.shop.controller.user.ao.UserInfoQueryAO;
+import com.corn.shop.controller.user.ao.UserLoginAO;
+import com.corn.shop.controller.user.ao.UserRegAO;
+import com.corn.shop.facade.user.*;
+import com.corn.shop.integration.user.UserFacadeClient;
+import com.corn.shop.integration.user.UserFacadeClientImpl;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.beans.BeanUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+
+@RequestMapping(value = "/user")
+@RestController
+@Api(description = "用户操作")
+public class UserController {
+
+    @Autowired
+    private UserFacadeClient userFacadeClient;
+
+    @Autowired
+    private UserFacadeClientImpl userFacadeClientI;
+
+    @PostMapping("/userLogin")
+    @ApiOperation(value = "用户登录",notes = "用户登陆接口")
+    public JsonResult userLogin(@RequestBody UserLoginAO userLoginAO){
+
+        UserLoginOrder order = new UserLoginOrder();
+        order.setSerialNo(AppUtils.appCode("userLogin"));
+        BeanUtils.copyProperties(userLoginAO,order);
+
+        UserLoginResult result = userFacadeClient.userLogin(order);
+        return new JsonResult(result);
+
+    }
+
+    @PostMapping("/userReg")
+    @ApiOperation(value = "用户注册",notes = "用户注册接口")
+    public JsonResult userReg(@RequestBody UserRegAO userRegAO){
+
+        UserRegOrder order = new UserRegOrder();
+        order.setSerialNo(AppUtils.appCode("userReg"));
+        order.setUserAccount(userRegAO.getUserAccount());
+        order.setUserName(userRegAO.getUserName());
+        order.setUserPassword(userRegAO.getUserPassword());
+        UserRegResult regResult = userFacadeClient.userReg(order);
+        return new JsonResult(regResult);
+
+    }
+
+    @GetMapping("/userInfoQuery")
+    @ApiOperation(value = "用户基础信息查询",notes = "用户基础信息查询接口")
+    public JsonResult userInfoQuery(UserInfoQueryAO ao){
+
+        UserInfoQueryOrder order = new UserInfoQueryOrder();
+        order.setUserId(ao.getUserId());
+        order.setSerialNo(AppUtils.appCode("userInfoQuery"));
+
+        UserInfoQueryResult result = userFacadeClient.userInfoQuery(order);
+        return new JsonResult(result);
+    }
+
+    @GetMapping("/test")
+    @ApiOperation(value = "test",notes = "test")
+    public void userInfoQuery(){
+
+        userFacadeClientI.userListQuery();
+    }
+}
---
+++ vworld-integration/src/main/java/com/corn/shop/integration/user/UserFacadeClientImpl.java
@@ -0,0 +1,58 @@
+package com.corn.shop.integration.user;
+
+import com.corn.shop.facade.user.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+
+/**
+ * @author yyc
+ * @apiNote 用户业务客户端
+ * */
+@Service
+public class UserFacadeClientImpl implements UserFacadeClient {
+
+    private static final Logger log = LoggerFactory.getLogger(UserFacadeClient.class);
+
+    @Autowired
+    private UserFacade userFacade;
+
+
+    @Override
+    public UserLoginResult userLogin(UserLoginOrder order) {
+        log.info("业务[用户登录],入参:{}",order);
+        UserLoginResult result = userFacade.userLogin(order);
+        log.info("业务[用户登录],出参:{}",result);
+        return result;
+    }
+
+    @Override
+    public UserRegResult userReg(UserRegOrder order) {
+        log.info("业务[用户注册],入参:{}",order);
+        UserRegResult result = userFacade.userReg(order);
+        log.info("业务[用户注册],出参:{}",result);
+        return result;
+    }
+
+    @Override
+    public UserForgetPasswordResult userForget(UserForgetPasswordOrder order) {
+        log.info("业务[用户忘记密码],入参:{}",order);
+        UserForgetPasswordResult result = userFacade.passwordForget(order);
+        log.info("业务[用户忘记密码],出参:{}",result);
+        return result;
+    }
+
+    @Override
+    public UserInfoQueryResult userInfoQuery(UserInfoQueryOrder order) {
+        log.info("业务[用户基础信息查询],入参:{}",order);
+        UserInfoQueryResult result = userFacade.userInfoQuery(order);
+        log.info("业务[用户基础信息查询],出参:{}",result);
+        return result;
+    }
+
+    public void userListQuery(){
+        userFacade.testMP();
+    }
+}
