dc7aa4f2b5d572123565324fa50a6377699f5f68
Yyc <791621554@qq.com>
2019-05-28 15:09:19 +0800

    遗漏提交

---
+++ vworld-controller/src/main/java/com/corn/vworld/controller/filter/OrderCheckFilter.java
@@ -0,0 +1,63 @@
+package com.corn.vworld.controller.filter;
+
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+
+/**
+ * @author yyc
+ * @apiNote 参数校验过滤器
+ * */
+public class OrderCheckFilter implements Filter {
+
+    private static Logger log = LoggerFactory.getLogger(OrderCheckFilter.class);
+
+    private static final String DEFAULT_DATE_PARSE_STRING = "yyyy-MM-dd HH:mm:ss";
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+
+        log.info("********** 参数校验过滤器初始化 **********");
+    }
+
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+        HttpServletRequest request = (HttpServletRequest) servletRequest;
+        HttpServletResponse response = (HttpServletResponse) servletResponse;
+
+        System.out.println(request.getParameter("createTime"));
+        System.out.println(request.getParameter("endTime"));
+        String startTime = request.getParameter("createTime"); //开始时间
+        if(startTime != null && startTime != ""){
+            System.out.println(1);
+            String endTime = request.getParameter("endTime"); //结束时间
+            SimpleDateFormat format = new SimpleDateFormat(DEFAULT_DATE_PARSE_STRING);
+
+            try {
+                request.setAttribute("createTime",format.parse(startTime));
+                request.setAttribute("endTime",format.parse(endTime));
+
+                filterChain.doFilter(request,response);
+            } catch (ParseException e) {
+
+                log.info("参数拦截过滤器异常:{}",e.getMessage());
+            }
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    @Override
+    public void destroy() {
+
+        log.info("********** 参数校验过滤器销毁 **********");
+
+    }
+}
