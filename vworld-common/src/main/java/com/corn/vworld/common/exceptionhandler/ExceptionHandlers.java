0180c0e28b6f675ce231c70dd45406d5d4d2249e
Yyc <791621554@qq.com>
2019-09-24 17:41:22 +0800

    修改

--- vworld-common/src/main/java/com/corn/vworld/common/exceptionhandler/ExceptionHandlers.java
+++ vworld-common/src/main/java/com/corn/vworld/common/exceptionhandler/ExceptionHandlers.java
@@ -1,6 +1,7 @@
 package com.corn.vworld.common.exceptionhandler;
 
 
+import com.corn.boot.base.HttpBase;
 import com.corn.boot.base.JsonResult;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -27,7 +28,7 @@ public class ExceptionHandlers implements ErrorController {
     public JsonResult MethodArgumentNotValidExceptionHandler(HttpServletRequest request, MethodArgumentNotValidException e){
         BindingResult bindingResult = e.getBindingResult();
         log.info("请求接口失败{},失败原因:{}",request.getRequestURI(),bindingResult.getFieldError().getDefaultMessage());
-        return new JsonResult(JsonResult.FIAL_MSG,bindingResult.getFieldError().getDefaultMessage());
+        return new JsonResult(HttpBase.HTTP_RESPONSE_ERROR_CODE,bindingResult.getFieldError().getDefaultMessage());
     }
 
     @Override
