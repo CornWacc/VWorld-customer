4c370cc3e81deb3cc1f10e9f82f21af0c040e20c
Yyc <791621554@qq.com>
2019-10-30 19:28:55 +0800

    修改

--- vworld-assamble/src/main/java/com/corn/vworld/Main.java
+++ vworld-assamble/src/main/java/com/corn/vworld/Main.java
@@ -3,14 +3,15 @@ package com.corn.vworld;
 
 import com.corn.boot.annotations.Runner;
 import com.corn.boot.base.RunMain;
+import com.corn.boot.configuration.DataSourceAutoConfiguration;
+import com.corn.boot.configuration.DataSourceProperties;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
-import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.context.annotation.ImportResource;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Runner(port = "9250")
 @EnableSwagger2
-@EnableAutoConfiguration(exclude = DataSourceAutoConfiguration.class)
+@EnableAutoConfiguration(exclude = {org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class,DataSourceAutoConfiguration.class, DataSourceProperties.class})
 public class Main {
 
     public static void main(String[] args) {
